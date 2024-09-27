11d0484515b69b8488e8488785350f39332ba9f6
maydindev <maydin.dev@outlook.com>
2024-09-27 11:36:14 +0300

    "Updated globals.css, layout.js, page.js, and tailwind.config.js with new styles, components, and configurations."

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "custom-color" : "rgb(6,96,89)",
+        'dirty-white': 'rgba(255, 255, 255, 0.5)',
+      },
+      backgroundImage: {
+        'custom-image': "url(/Login5Background.png)",
+      },
+      backgroundSize: {
+        'size-200': '200%',
+        'size-150': '150%',
+        'custom-size': '1800px 1000px',
       },
     },
   },
